67e51a125b2865273d91d9dcedd7fb863e042004
Trevor L Davis <trevor.l.davis@gmail.com>
2021-05-21 11:54:13 -0700

    feat: Let 'crosshatch' pattern support 'fill2' parameter
    * `fill2` argument controls the fill of the "top" hatch
    * `fill2` defaults to the value of `fill`
    
    closes #14

--- R/pattern-geometry-crosshatch.R
+++ R/pattern-geometry-crosshatch.R
@@ -3,12 +3,14 @@
 #' `grid.pattern_crosshatch()` draws a crosshatch pattern onto the graphic device.
 #'
 #' @inheritParams grid.pattern_circle
+#' @param fill2 The fill colour for the \dQuote{top} crosshatch lines.
 #' @return A grid grob object invisibly.  If `draw` is `TRUE` then also draws to the graphic device as a side effect.
 #' @examples
 #'   if (require("grid")) {
 #'     x_hex <- 0.5 + 0.5 * cos(seq(2 * pi / 4, by = 2 * pi / 6, length.out = 6))
 #'     y_hex <- 0.5 + 0.5 * sin(seq(2 * pi / 4, by = 2 * pi / 6, length.out = 6))
-#'     grid.pattern_crosshatch(x_hex, y_hex, colour = "green", fill = "blue", density = 0.5)
+#'     grid.pattern_crosshatch(x_hex, y_hex, colour = "black", fill = "blue",
+#'                             fill2 = "yellow", density = 0.5)
 #'     grid.newpage()
 #'     grid.pattern_crosshatch(x_hex, y_hex, density = 0.3,
 #'                             gp = gpar(col = "blue", fill = "yellow"))
@@ -17,13 +19,15 @@
 #' @export
 #' @seealso
 grid.pattern_crosshatch <- function(x = c(0, 0, 1, 1), y = c(1, 0, 0, 1), id = 1L, ...,
-                                    colour = gp$col %||% "grey20", fill = gp$fill %||% "grey80", angle = 30,
-                                    density = 0.2, spacing = 0.05, xoffset = 0, yoffset = 0,
+                                    colour = gp$col %||% "grey20",
+                                    fill = gp$fill %||% "grey80", fill2 = fill,
+                                    angle = 30, density = 0.2,
+                                    spacing = 0.05, xoffset = 0, yoffset = 0,
                                     alpha = gp$alpha %||% 1, linetype = gp$lty %||% 1, size = gp$lwd %||% 1,
                                     default.units = "npc", name = NULL, gp = gpar(), draw = TRUE, vp = NULL) {
     if (missing(colour) && hasName(l <- list(...), "color")) colour <- l$color
     grid.pattern("crosshatch", x, y, id,
-                 colour = colour, fill = fill, angle = angle,
+                 colour = colour, fill = fill, fill2 = fill2, angle = angle,
                  density = density, spacing = spacing, xoffset = xoffset, yoffset = yoffset,
                  alpha = alpha, linetype = linetype, size = size,
                  default.units = default.units, name = name, gp = gp , draw = draw, vp = vp)
@@ -67,34 +71,24 @@ create_pattern_crosshatch_via_sf <- function(params, boundary_df, aspect_ratio,
   stripe_polygons2 <- convert_polygon_sf_to_polygon_df(striped_area)
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Join all stripe polygons, and make sure the IDs don't overlap
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  if (is.null(stripe_polygons1) && is.null(stripe_polygons2)) {
-    return(grid::nullGrob())
-  } else if (is.null(stripe_polygons1)) {
-    crosshatch_polygons  <- stripe_polygons2
-  } else if (is.null(stripe_polygons2)) {
-    crosshatch_polygons  <- stripe_polygons1
-  } else {
-    stripe_polygons2$id <- stripe_polygons2$id + max(stripe_polygons1$id)
-    crosshatch_polygons  <- rbind(stripe_polygons1, stripe_polygons2)
-  }
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # Create the grob and return
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  crosshatch_grob <- grid::polygonGrob(
-    x = unit(crosshatch_polygons$x, "npc"),
-    y = unit(crosshatch_polygons$y, "npc"),
-    id = crosshatch_polygons$id,
-    gp = gpar(
-      col     = alpha(params$pattern_colour, params$pattern_alpha),
-      fill    = alpha(params$pattern_fill  , params$pattern_alpha),
-      lwd     = params$pattern_size * .pt,
-      lty     = params$pattern_linetype,
-      lineend = 'square'
-    )
-  )
+  under_hatch <- as_crosshatch_grob(stripe_polygons1, params, fill = params$pattern_fill)
+  over_hatch  <- as_crosshatch_grob(stripe_polygons2, params, fill = params$pattern_fill2)
+  gList(under_hatch, over_hatch)
+}
 
-  crosshatch_grob
+as_crosshatch_grob <- function(stripe_polygons, params, fill = params$pattern_fill) {
+    if (is.null(stripe_polygons)) return(grid::nullGrob())
+    grid::polygonGrob(x = unit(stripe_polygons$x, "npc"),
+      y = unit(stripe_polygons$y, "npc"),
+      id = stripe_polygons$id,
+      gp = gpar(
+        col     = alpha(params$pattern_colour, params$pattern_alpha),
+        fill    = alpha(fill, params$pattern_alpha),
+        lwd     = params$pattern_size * .pt,
+        lty     = params$pattern_linetype,
+        lineend = 'square'
+      )
+    )
 }
